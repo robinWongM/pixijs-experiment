be16e4ed83a4e5f4628b6d401cccdf98be23cee1
robinWongM <chaowang0313@gmail.com>
2024-02-04 00:25:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pixijs-experiment
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/robinWongM/pixijs-experiment)
---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import * as PIXI from 'pixi.js';
+
+const element = document.getElementById('test');
+const canvas = element;
+
+const wrap = (canvas) => {
+  const originAddEventListener = canvas.addEventListener.bind(canvas);
+  canvas.addEventListener = (...args) => {
+    args[2] = { capture: args[2] === false ? false : true, passive: true };
+    originAddEventListener(...args);
+  };
+};
+
+wrap(canvas);
+wrap(globalThis.document);
+
+const app = new PIXI.Application({
+  background: '#1099bb',
+  resizeTo: window,
+  view: canvas,
+});
+
+let isFlower = true;
+
+const texture = PIXI.Texture.from('https://pixijs.com/assets/flowerTop.png');
+const secondTexture = PIXI.Texture.from(
+  'https://pixijs.com/assets/eggHead.png'
+);
+
+// create a new Sprite using the texture
+const character = new PIXI.Sprite(texture);
+
+// center the sprites anchor point
+character.anchor.set(0.5);
+
+// move the sprite to the center of the screen
+character.x = app.screen.width / 2;
+character.y = app.screen.height / 2;
+
+app.stage.addChild(character);
+
+// make the sprite interactive
+character.eventMode = 'static';
+character.cursor = 'pointer';
+
+character.on('pointertap', () => {
+  isFlower = !isFlower;
+  // Dynamically swap the texture
+  character.texture = isFlower ? texture : secondTexture;
+});
+
+app.ticker.add(() => {
+  character.rotation += 0.02;
+});
+
+try {
+  app.view.style.touchAction = '';
+} catch (e) {}
